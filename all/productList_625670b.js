528554bc4ecc7ea62ac2a86b73ca03945e8ca7eb
leewinmen <leewinmen@qq.com>
2020-02-24 16:29:01 +0800

    all

---
+++ all/productList_625670b.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper(".products-swiper .swiper-container",{slidesPerView:"auto"});$(".products-swiper .prev").on("click",function(){swiper.swipePrev()}),$(".products-swiper .next").on("click",function(){swiper.swipeNext()});
